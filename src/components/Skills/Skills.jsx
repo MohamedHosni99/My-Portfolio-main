6fed66280db21f02e4d5dbcb5ed26695a5da5ea6
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2024-03-27 00:15:30 +0200

    Update Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import './Skills.css';
-import react from '../../assets/React .png';
-import Redux from '../../assets/Redux.png';
-import Bootsrap from '../../assets/Bootstrap .png';
-import Git_Hub from '../../assets/Git_Hub.png';
-import Tailwind from '../../assets/Tailwind.png';
-import HTML from '../../assets/HTML.png';
-import CSS from '../../assets/css.png';
-import JS from '../../assets/JS .png';
-import Sass from '../../assets/Sass.png';
-import Vs_Code from '../../assets/VS.png';
 import{motion} from 'framer-motion';
 import {fadeIn} from '../../../src/variants';
+import {skillsData}  from '../../data'
 
 
 const Skills = () => {
@@ -22,72 +13,26 @@ const Skills = () => {
     whileInView={'show'}
     viewport={{once: false, amount: 0.3}}
     className='main-title h3 '>Skills & Tools</motion.h1>
+    
     <motion.div
-    variants={fadeIn('up', 0.3)}
-    initial='hidden'
-    whileInView={'show'}
-    viewport={{once: false, amount: 0.3}}
-    className='skills-container grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 p-10'>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={HTML}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={CSS}/></motion.div>
-          <motion.div 
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={JS}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={Sass}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={Bootsrap}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={Tailwind}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={react}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={Redux}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={Vs_Code}/></motion.div>
-          <motion.div
-          variants={fadeIn('up', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once: false, amount: 0.3}} 
-          className='box'><img src={Git_Hub}/></motion.div>
+      variants={fadeIn('up', 0.3)}
+      initial='hidden'
+      whileInView={'show'}
+      viewport={{once: false, amount: 0.3}}
+      className='skills-container grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 p-10'>
+      {skillsData.map((skill,index) => (
+         <motion.div
+         variants={fadeIn('up', 0.3)}
+         key={index}
+         initial='hidden'
+         whileInView={'show'}
+         viewport={{once: false, amount: 0.3}} 
+         className='box'>
+         <img src={skill.image} alt={skill.name}/>
+       </motion.div>
+      ))
+      }
+      
     </motion.div>
     </section>;
 };
