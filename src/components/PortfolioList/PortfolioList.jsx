e75b87746fd390fc666d113d530c91e8d570c7d7
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2024-04-22 01:17:11 +0200

    Update PortfolioList.jsx

--- src/components/PortfolioList/PortfolioList.jsx
+++ src/components/PortfolioList/PortfolioList.jsx
@@ -1,14 +1,14 @@
-import './PortfolioList.css'
+import './PortfolioList.css';
 
-function PortfolioList({ id, title, active, setSelected }) {
-    return (
-      <li 
-        className= {active ? "portfolioList active h3" : "portfolioList h3"} 
-        onClick={() => setSelected(title)}
-      >
-        {title}
-      </li>
-    );
-  }
-  
+function PortfolioList({ title, active, setSelected }) {
+  return (
+    <li 
+      className={active ? "portfolioList active h3" : "portfolioList h3"} 
+      onClick={() => setSelected(title)} 
+    >
+      {title}
+    </li>
+  );
+}
+
 export default PortfolioList;
