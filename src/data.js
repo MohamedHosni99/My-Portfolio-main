b9768ccf2c53d112e9d45753e684599584fc5b7f
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2024-04-22 01:10:49 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,6 +6,11 @@ import portfolio_5 from './assets/portfolio_5.png';
 import portfolio_6 from './assets/portfolio_6.png';
 import portfolio_7 from './assets/portfolio_7.png';
 import portfolio_8 from './assets/portfolio_8.png';
+import portfolio_9 from './assets/portfolio_9.png';
+import portfolio_10 from './assets/portfolio_10.png';
+import portfolio_11 from './assets/portfolio_11.png';
+import portfolio_12 from './assets/portfolio_12.png';
+import portfolio_13 from './assets/portfolio_13.png';
 
 import react from './assets/React .png';
 import Redux from './assets/Redux.png';
@@ -21,6 +26,7 @@ import Vue from './assets/VueJs.png';
 import Mui from './assets/MUI.png';
 import Figma from './assets/Figma.png';
 import Js from './assets/JS.png';
+import Ts from './assets/TS.png';
 
 
 
@@ -32,6 +38,7 @@ export const AllPortfolio = [
         img:portfolio_1,
         link:"https://mohamedhosni99.github.io/Dashboard_HTML-CSS/",
         git_link:"https://github.com/MohamedHosni99/Dashboard_HTML-CSS",
+        Category:"HTML&CSS"
     },
     {
         id: 2,
@@ -39,6 +46,7 @@ export const AllPortfolio = [
         img:portfolio_2,
         link:"https://mohamedhosni99.github.io/Leon_Template1-HTML-CSS-/",
         git_link:"https://github.com/MohamedHosni99/Leon_Template1-HTML-CSS-",
+        Category:"HTML&CSS"
     },
     {
         id: 3,
@@ -46,123 +54,98 @@ export const AllPortfolio = [
         img:portfolio_3,
         link:"https://mohamedhosni99.github.io/Huddle_Template-HTML-CSS-/",
         git_link:"https://github.com/MohamedHosni99/Huddle_Template-HTML-CSS-",
+        Category:"HTML&CSS"
     },
     {
         id: 4,
-        title: "Bondi Website",
-        img: portfolio_4,
-        link:"https://mohamedhosni99.github.io/Bondi-BootStrap--Project/",
-        git_link:"https://github.com/MohamedHosni99/Bondi-BootStrap--Project"
+        title: "Kasper Website",
+        img:portfolio_4,
+        link:"https://mohamedhosni99.github.io/Kasper_Template2/",
+        git_link:"https://github.com/MohamedHosni99/Kasper_Template2",
+        Category:"HTML&CSS"
     },
     {
         id: 5,
+        title: "Bondi Website",
+        img: portfolio_5,
+        link:"https://mohamedhosni99.github.io/Bondi-BootStrap--Project/",
+        git_link:"https://github.com/MohamedHosni99/Bondi-BootStrap--Project",
+        Category:"BootStrap"
+    },
+    {
+        id: 6,
         title: "Elzero Website",
-        img:portfolio_5,
+        img:portfolio_6,
         link:"https://mohamedhosni99.github.io/Elzero_Template-JS/",
         git_link:"https://github.com/MohamedHosni99/Elzero_Template-JS",
+        Category:"HTML&CSS&JS"
     },
         {
-        id: 6,
+        id: 7,
         title: "Special Design Website",
-        img:portfolio_6,
+        img:portfolio_7,
         link:"https://mohamedhosni99.github.io/Special-Design/",
         git_link:"https://github.com/MohamedHosni99/Special-Design",
-        },
-        {
-            id: 7,
-            title: "Travel Website",
-            img: portfolio_7 ,
-            link:"https://mohamedhosni99.github.io/Travel-Project-HTML-SASS-JS-/",
-            git_link:"https://github.com/MohamedHosni99/Travel-Project-HTML-SASS-JS-",
+        Category:"HTML&CSS&JS"
         },
         {
             id: 8,
+            title: "Travel Website",
+            img: portfolio_8,
+            link:"https://mohamedhosni99.github.io/Travel-Project-HTML-SASS-JS-/",
+            git_link:"https://github.com/MohamedHosni99/Travel-Project-HTML-SASS-JS-",
+            Category:"HTML&SASS&JS"
+        },
+        {
+            id: 9,
             title: "Acme-Rockets Website",
-            img:portfolio_8,
+            img:portfolio_9,
             link:"https://acme-rockets-lfar.onrender.com/",
             git_link:"https://github.com/MohamedHosni99/Acme-Rocket-TailwindCSS",
+            Category:"Tailwindcss"
         },
+        {
+            id: 10,
+            title: "Weather-App",
+            img:portfolio_10,
+            link:"https://weather-app-ivory-omega-92.vercel.app/",
+            git_link:"https://github.com/MohamedHosni99/Weather-App-Vue-js-",
+            Category:"Vue.js"
+        },
+        {
+            id: 11,
+            title: "Fitness-App",
+            img:portfolio_11,
+            link:"https://fitness-app-phi-ten.vercel.app/",
+            git_link:"https://github.com/MohamedHosni99/Fitness-App",
+            Category:"React.js"
+        },
+        
+        {
+            id: 12,
+            title: "Youtube-Clone",
+            img:portfolio_12,
+            link:"https://youtube-clone-beta-azure.vercel.app/",
+            git_link:"https://github.com/MohamedHosni99/Youtube-Clone",
+            Category:"React.js"
+        },
+
+        {
+            id: 13,
+            title: "MetaVerse",
+            img:portfolio_13,
+            link:"https://metaverse-alpha-lake.vercel.app/",
+            git_link:"https://github.com/MohamedHosni99/Metaverse",
+            Category:"Next.js"
+        },
+        
+
     
 ]
 
 
 
 
-
-
-export const HTML_CSSPortfolio = [
-    {
-        id: 1,
-        title: "Dashboard Website",
-        img: portfolio_1,
-        link:"https://mohamedhosni99.github.io/Dashboard_HTML-CSS/",
-        git_link:"https://github.com/MohamedHosni99/Dashboard_HTML-CSS",
-    },
-    {
-        id: 2,
-        title: "Leon Website",
-        img:portfolio_2,
-        link:"https://mohamedhosni99.github.io/Leon_Template1-HTML-CSS-/",
-        git_link:"https://github.com/MohamedHosni99/Leon_Template1-HTML-CSS-",
-    },
-    {
-        id: 3,
-        title: "Huddle Website",
-        img:portfolio_3,
-        link:"https://mohamedhosni99.github.io/Huddle_Template-HTML-CSS-/",
-        git_link:"https://github.com/MohamedHosni99/Huddle_Template-HTML-CSS-",
-    },
-];
-
-export const HTML_CSS_JSPortfolio = [
-    {   
-        id: 1,
-        title: "Elzero Website",
-        img:portfolio_5,
-        link:"https://mohamedhosni99.github.io/Elzero_Template-JS/",
-        git_link:"https://github.com/MohamedHosni99/Elzero_Template-JS",
-    },
-        {
-            id: 2,
-            title: "Special Design Website",
-            img:portfolio_6,
-            link:"https://mohamedhosni99.github.io/Special-Design/",
-            git_link:"https://github.com/MohamedHosni99/Special-Design",
-        },
-    ];
-  
-  export const HTML_SASS_JSPortfolio = [
-    {
-        id: 1,
-        title: "Travel Website",
-        img: portfolio_7 ,
-        link:"https://mohamedhosni99.github.io/Travel-Project-HTML-SASS-JS-/",
-        git_link:"https://github.com/MohamedHosni99/Travel-Project-HTML-SASS-JS-",
-    },
-    
-  ];
-  
-  export const BootsrapPortfolio = [
-    {
-        id: 4,
-        title: "Bondi Website",
-        img: portfolio_4,
-        link:"https://mohamedhosni99.github.io/Bondi-BootStrap--Project/",
-        git_link:"https://github.com/MohamedHosni99/Bondi-BootStrap--Project"
-    },
-    
-  ];
-  
-  export const TailwindPortfolio = [
-    {
-        id: 8,
-            title: "Acme-Rockets Website",
-            img:portfolio_8,
-            link:"https://acme-rockets-lfar.onrender.com/",
-            git_link:"https://github.com/MohamedHosni99/Acme-Rocket-TailwindCSS",
-    },
-
-];
 
 
 export const skillsData = [
@@ -179,7 +162,8 @@ export const skillsData = [
     { name: 'Vue', image: Vue },
     { name: 'Figma', image: Figma },
     { name: 'GitHub', image: Git_Hub },
-    { name: 'VS Code', image: Vs_Code }
+    { name: 'VS Code', image: Vs_Code },
+    { name: 'TypeScript', image: Ts },
 ];
 
 
