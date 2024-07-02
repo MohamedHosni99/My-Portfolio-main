cf2712a71a0fe66a319ff2a5f56ffe7d8f6dadcc
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2024-07-02 22:08:17 +0300

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -50,7 +50,7 @@ const Banner = () => {
             whileInView={"show"}
             viewport={{ once: false, amount: 0.7 }}
             className="cv mt-8 ">
-            <div className="btn-download text-base sm:text-2xl capitalize"><a href="https://drive.google.com/file/d/1aVboTWOMYkL78L--vlDAnh2Kf5lRKLmu/view?usp=drive_link" target='blank'>Download CV</a></div>
+            <div className="btn-download text-base sm:text-2xl capitalize"><a href="https://drive.google.com/file/d/1Q85-_NpWMIPJXdFfGNYOkak7-b1dufCL/view?usp=drivesdk" target='blank'>Download CV</a></div>
             </motion.div>
             
             <motion.div
